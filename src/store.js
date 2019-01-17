8905b51def5260ae33937a5914b33f7010201e5d
ameenrj <ameen.ramadan.jradi@gmail.com>
2019-01-17 18:07:38 +1100

    feat(menu): view recipes under a meal plan

--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    recipes: [],
+    apiUrl: 'https://api.edamam.com/search'
+  },
+  mutations: {
+    setRecipes(state, payload) {
+      state.recipes = payload;
+    }
+  },
+  actions: {
+    async getRecipes({ state, commit }, plan) {
+      try {
+        const response = await axios.get(`${state.apiUrl}`, {
+          params: {
+            q: plan,
+            app_id: 'b15e4a71',
+            app_key: '14166b81e443f95154249ff537d1e590',
+            from: 0,
+            to: 9
+          }
+        });
+        commit('setRecipes', response.data.hits);
+      } catch (error) {
+        commit('setRecipes', []);
+      }
+    }
+  }
 });
